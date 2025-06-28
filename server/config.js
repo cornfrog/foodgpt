0a2d81770af7c33907fac4bbcc0d0748f176ae1b
Corn <cornfrog@gmail.com>
2025-06-27 22:25:40 -0400

    gemini interaction work....need to combine with google maps api

--- server/config.js
+++ server/config.js
@@ -3,5 +3,5 @@ import { config } from "dotenv";
 config();
 
 export const SERVER_CONFIG = {
-    
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
 import express from "express";
+import cors from 'cors';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { SERVER_CONFIG } from "./config.js";
 
 const server = express();
+server.use(cors())
+server.use(express.json())
 
-server.get("/", (request, response) => {
-    return response.json({
-        message: "server running..."
-    })
-})
+
+
+const interactWithGemini = async () => {
+    const genAI = new GoogleGenerativeAI(SERVER_CONFIG.GEMINI_API_KEY);
+    const prompt = "how do i connect the google map api to you?"
+    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+    const result = await model.generateContent(prompt);
+    const response = result.response;
+    console.log(response.text())
+
+}
+
+interactWithGemini();
+
+// server.get("/", (request, response) => {
+//     return response.json({
+//         message: "server running..."
+//     })
+// });
 
 server.listen(3001, () => console.log('Server running on port 3001'));
